a3a259f15d423a96281aefc5a5b61d9f84348e27
Dror Atariah <d.atariah@rebuy.com>
2018-02-09 11:58:52 +0100

    Added short project name

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='{{ cookiecutter.repo_name }}',
+setup(name='{{ cookiecutter.short_prj_name }}',
       version='POC',
       url='https://github.com/rebuy-de/{{ cookiecutter.repo_name }}.git',
       author='{{ cookiecutter.author_name }}',
