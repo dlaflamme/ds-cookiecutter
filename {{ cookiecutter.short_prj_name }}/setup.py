68da55c7aad439eeb4c8d658e33c3d9ac2296ad1
Dror Atariah <drorata@gmail.com>
2018-03-22 16:21:03 +0100

    Removed ref to reBuy in the setup

--- {{ cookiecutter.short_prj_name }}/setup.py
+++ {{ cookiecutter.short_prj_name }}/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='{{ cookiecutter.short_prj_name }}',
       version='POC',
-      url='https://github.com/rebuy-de/{{ cookiecutter.repo_name }}.git',
+      url='https://github.com/{{ cookiecutter.github }}/{{ cookiecutter.repo_name }}.git',
       author='{{ cookiecutter.author_name }}',
       author_email='{{ cookiecutter.email }}',
       packages=['src'],
